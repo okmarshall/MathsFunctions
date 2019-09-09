991b287914d4f97ae839a26ffa10e7a25b37ea55
okmarshall <oliverkmarshall@gmail.com>
2019-09-09 18:02:20 +0100

    Initial commit to development

---
+++ MathsFunctions.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathsFunctions.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
